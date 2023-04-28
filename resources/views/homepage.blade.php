383dc59330b03a3ffe75fa7cb11ebbc5265eef0e
Mkharzom <76581486+Mkharzom@users.noreply.github.com>
2023-04-28 17:23:10 +0800

    minior update

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,25 @@
+@extends('layout.app')
+
+@section('content')
+
+    @include('homepage.carousel')
+
+    @include('homepage.banner')
+
+    @include('homepage.about')
+
+    @include('homepage.appointment')
+
+    @include('homepage.service')
+
+    @include('homepage.offer')
+
+    @include('homepage.pricing')
+
+    @include('homepage.testimonial')
+
+    @include('homepage.team')
+
+    @include('homepage.newsletter')
+ 
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Route::group(['prefix' => 'admin'], function () {
     Voyager::routes();
 });
+
+
+Route::get('/', [HomeController::class, 'index'])->name('homepage');
